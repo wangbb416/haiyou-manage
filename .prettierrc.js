c40f5aa4fdb48462791a144d8f27498d31c6ad7b
wangbin <108792642+wangbb416@users.noreply.github.com>
2024-02-21 17:56:19 +0800

    Add files via upload

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  printWidth: 200,
+  pugSingleQuote: false,
+  pugPrintWidth: 999,
+  pugEmptyAttributes: 'none',
+  pugFramework: 'vue',
+}
